fbb49815a3033614a8f45d54c95a4495b58b94b8
Siripod Surabotsophon <siripods@gmail.com>
2021-01-16 23:09:14 +0700

    rabbitmq tutorial

---
+++ ReceiveLogsDirect.java
@@ -0,0 +1,42 @@
+import com.rabbitmq.client.*;
+
+public class ReceiveLogsDirect {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        String queueName = channel.queueDeclare().getQueue();
+channel.basicQos(1);
+        if (argv.length < 1) {
+            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+            System.exit(1);
+        }
+
+        for (String severity : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+			try {
+                    Thread.sleep(5000);
+					System.out.println(" [x] Done");
+					//channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
+
---
+++ setclasspath.bat
@@ -0,0 +1,1 @@
+set CP=.;amqp-client-5.6.0.jar;slf4j-api-1.7.26.jar;slf4j-simple-1.7.26.jar
